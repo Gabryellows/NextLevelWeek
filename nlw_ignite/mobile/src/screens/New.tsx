2f75edd50dfea7ac9caf8f8aefdb3852b1b0c33c
Gabriel Leal <gabryellows@gmail.com>
2023-01-20 18:06:44 -0300

    return caso o usuario n preencha nenhum campo

--- nlw_ignite/mobile/src/screens/New.tsx
+++ nlw_ignite/mobile/src/screens/New.tsx
@@ -20,22 +20,24 @@ export function New() {
         }
     }
 
-    async function handleCreateNewHabit () {
+    async function handleCreateNewHabit() {
         try {
             if(!title.trim() || weekDays.length === 0) {
                 Alert.alert('Novo hábito', 'Informe o nome do hábito e escolhe a periodicidade')
+                return;
             }
 
-            await api.post('/habits', {title, weekDays});
-            setTitle('');
-            setWeekDays([]);
-
-            Alert.alert('Novo hábito', 'Hábito criado com sucesso!')
+          await api.post('/habits', { title, weekDays })
+          
+          setTitle('');
+          setWeekDays([]);
+    
+          Alert.alert('Novo hábito', 'Hábito criado com sucesso!');
         } catch (error) {
-            console.log(error);
-            Alert.alert('Ops', 'Não foi possivel criar o novo hábito')
+          console.log(error)
+          Alert.alert('Ops', 'Não foi possível criar o novo hábito')
         }
-    }
+      }
 
     return (
         <View className="flex-1 bg-background px-8 pt-16">
@@ -73,7 +75,7 @@ export function New() {
                     ))
                 }
 
-                <TouchableOpacity className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6" activeOpacity={0.7}>
+                <TouchableOpacity className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6" activeOpacity={0.7} onPress={handleCreateNewHabit}>
                     <Feather 
                         name="check"
                         size={20}
