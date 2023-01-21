21e4cd9acae62fea958448e38329f1fe2c630678
Gabriel Leal <gabryellows@gmail.com>
2023-01-21 10:57:07 -0300

    criação do componente HabitsList e alteração da progressBar

--- nlw_ignite/web/src/components/SummaryTable.tsx
+++ nlw_ignite/web/src/components/SummaryTable.tsx
@@ -44,7 +44,7 @@ export function SummaryTable () {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                {SummaryDates.map(date => {
+                {summary.length > 0 && SummaryDates.map(date => {
                     const dayInSummary = summary.find(day => {
                         return dayjs(date).isSame(day.date, 'day');
                     })
@@ -55,7 +55,7 @@ export function SummaryTable () {
                         key={date.toString()}
                         date={date}
                         amount={dayInSummary?.amount} 
-                        completed={dayInSummary?.completed} 
+                        defaultCompleted={dayInSummary?.completed} 
                     />
                 )
             })}
