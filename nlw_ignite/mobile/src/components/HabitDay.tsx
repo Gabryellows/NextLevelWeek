3e8dfeb5962bf8983cab514a6ad593ac690c989f
Gabriel Leal <gabryellows@gmail.com>
2023-01-22 18:12:05 -0300

    Finalização parte mobile aula 5

--- nlw_ignite/mobile/src/components/HabitDay.tsx
+++ nlw_ignite/mobile/src/components/HabitDay.tsx
@@ -1,41 +1,42 @@
-import { TouchableOpacity, Dimensions, TouchableOpacityProps } from "react-native";
-import { generateProgressPercentage } from "../utils/generate-progress-percentage";
+import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
 import clsx from "clsx";
+
+import { generateProgressPercentage } from "../utils/generate-progress-percentage";
 import dayjs from "dayjs";
 
 const WEEK_DAYS = 7;
 const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
 
 export const DAY_MARGIN_BETWEEN = 8;
-export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
+export const DAY_SIZE =  (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
 
+interface Props extends TouchableOpacityProps {
+  amountOfHabits?: number;
+  amountCompleted?: number;
+  date: Date;
+}
 
-interface HabitProps extends TouchableOpacityProps {
-    amountOfHabits?: number;
-    amountCompleted?: number;
-    data: Date;
-};
+export function HabitDay({ amountOfHabits = 0, amountCompleted = 0, date, ...rest }: Props) {
+  const amountAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountOfHabits, amountCompleted) : 0
+  const today = dayjs().startOf('day').toDate();
+  const isCurrentDay = dayjs(date).isSame(today);
 
-export function HabitDay({amountOfHabits = 0, amountCompleted = 0, data, ...rest}: HabitProps) {
-    
-    const amoutAccomplishedPercentage = amountOfHabits > 0 ? generateProgressPercentage(amountCompleted, amountCompleted) : 0
-    const today = dayjs().startOf('day').toDate();
-    const isCurrentDay = dayjs(data).isSame(today);
-
-    return (
-        <TouchableOpacity 
-        className={clsx("rounded-lg border-2 m-1", {
-            ["bg-zinc-900 border-zinc-800"] : amoutAccomplishedPercentage === 0,
-            ["bg-violet-500 border-violet-300"] : amoutAccomplishedPercentage > 0 && amoutAccomplishedPercentage < 20,
-            ["bg-violet-600 border-violet-400"] : amoutAccomplishedPercentage >= 20 && amoutAccomplishedPercentage < 40,
-            ["bg-violet-700 border-violet-500"] : amoutAccomplishedPercentage >= 40 && amoutAccomplishedPercentage < 60,
-            ["bg-violet-800 border-violet-600"] : amoutAccomplishedPercentage >= 60 && amoutAccomplishedPercentage < 80,
-            ["bg-violet-900 border-violet-700"] : amoutAccomplishedPercentage >= 80,
-            ["border-white border-4"] : isCurrentDay
-        })}
-        style={{width: DAY_SIZE, height: DAY_SIZE}}
-        activeOpacity={0.7}
-        {...rest}
-        />
-    );
+  return (
+    <TouchableOpacity 
+      className={clsx(
+        "rounded-lg border-2 m-1", {
+          ["bg-zinc-900 border-zinc-800"] : amountAccomplishedPercentage === 0,
+          ["bg-violet-900 border-violet-700"] : amountAccomplishedPercentage > 0 && amountAccomplishedPercentage < 20,
+          ["bg-violet-800 border-violet-600"] : amountAccomplishedPercentage >= 20 && amountAccomplishedPercentage < 40,
+          ["bg-violet-700 border-violet-500"] : amountAccomplishedPercentage >= 40 && amountAccomplishedPercentage < 60,
+          ["bg-violet-600 border-violet-500"] : amountAccomplishedPercentage >= 60 && amountAccomplishedPercentage < 80,
+          ["bg-violet-500 border-violet-400"] : amountAccomplishedPercentage >= 80,
+          ["border-white border-4"] : isCurrentDay,
+        } 
+      )}
+      style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      activeOpacity={0.7}
+      {...rest}
+    />
+  )
 }
--- nlw_ignite/mobile/src/components/HabitEmpty.tsx
+++ nlw_ignite/mobile/src/components/HabitEmpty.tsx
@@ -1,22 +1,18 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text } from "react-native";
-import { useNavigation } from '@react-navigation/native'
 
-interface HabitEmptyProps {}
+export function HabitsEmpty() {
+  const { navigate } = useNavigation()
 
-export function HabitEmpty(props: HabitEmptyProps) {
-
-    const { navigate } = useNavigation();
-
-    return (
-        <Text className="text-zinc-400 text-base">
-            Você ainda não está monitorando nenhum hábito {' '}
-
-            <Text 
-                className="text-violet-400 text-base underline active:text-violet-500"
-                onPress={() => navigate('new')}
-            >
-                comece registrando um
-            </Text>
-        </Text>
-    )
+  return (
+    <Text className="text-zinc-400 text-base">
+      Você ainda não está monitorando nenhum hábito {' '}
+      <Text
+        className="text-violet-400 text-base underline active:text-violet-500"
+        onPress={() => navigate('new')}
+      >
+        comece cadastrando um.
+      </Text>
+    </Text>
+  )
 }
